213215472adc4ff940e428977fb76d84ad62e29c
Chong-Cui <2589587C@student.gla.ac.uk>
2022-02-24 12:40:35 +0000

    chapter9 finished without fail

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 from turtle import title
 from xml.dom import expatbuilder
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
@@ -21,7 +22,7 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
-    
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -29,3 +30,18 @@ class PageForm(forms.ModelForm):
             url = f'https://{url}'
             cleaned_data = url
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
